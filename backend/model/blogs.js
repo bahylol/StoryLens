be2f34f1e5e4c27a08cf0a4200f616b5ac805daa
bahylol <bahymohamed2010@gmail.com>
2024-11-20 15:25:47 +0100

    added database model

---
+++ backend/model/blogs.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    imagelink: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  }
+);
+
+module.exports = mongoose.model("blogSchema", blogSchema);
