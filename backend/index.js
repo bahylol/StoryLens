2fac1f5e209d4f6dcd5c1960bc68a64980c7edfd
bahylol <bahymohamed2010@gmail.com>
2024-12-06 14:37:45 +0100

    Added the routes and database connection

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,24 @@
 require("dotenv").config();
 const express = require("express");
- 
+const db = require("./config/database.js");
+const blogsRouter = require("./routes/blogs.js");
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.use("/", blogsRouter);
 app.get("/health", (req, res) => {
-	res.send("Server is running up and healthy");
-  });
-  
+    res.send("Server is running up and healthy");
+});
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+db.once("open", () => {
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+});
+
+db.on("error", (err) => {
+    console.error("MongoDB error:", err);
+});
