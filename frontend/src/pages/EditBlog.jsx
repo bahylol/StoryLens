fff1ae9f6d2d8560a7ee72be92e94049f4adcfc3
bahylol <bahymohamed2010@gmail.com>
2025-01-13 17:28:19 +0100

    Created edit page forntend

--- frontend/src/pages/EditBlog.jsx
+++ frontend/src/pages/EditBlog.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import FormCreator from '../components/FormCreator';
+import formStyle from '../formTemplates/formStyle.json';
+import formTemplate from '../formTemplates/formTemplate.json';
 
 function EditBlog() {
   return (
-    <div>
-      <h1>Edit a Blog</h1>
-      <p>Here you can edit a blog post.</p>
+    <div className="flex items-center justify-center bg-gradient-to-b from-blue-200 to-gray-200">
+      <div className="m-8">
+        <FormCreator
+          formTemplate={formTemplate}
+          formStyle={formStyle}
+          onSubmit={() => console.log('hi')}
+        />
+      </div>
     </div>
   );
 }
