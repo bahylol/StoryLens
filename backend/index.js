261a8c4666e9e51ce044f8b91884f678576576d5
bahylol <bahymohamed2010@gmail.com>
2025-01-14 14:01:46 +0100

    added cors to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const db = require("./config/database.js");
 const blogsRouter = require("./routes/blogs.js");
 
 const app = express();
+
+// Configure CORS options
+const corsOptions = {
+    origin: [process.env.FRONTEND], // Allow specific origins
+    methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
+};
+
+// Enable CORS with the configured options
+app.use(cors(corsOptions));
+
+// Parse incoming JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
